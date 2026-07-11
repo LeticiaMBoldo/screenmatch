572cd04ef7aa226fa5c7acbb731d6b20b3e34829
Leticia Boldo <leticiamboldo14@gmail.com>
2026-07-11 11:21:31 -0300

    Início do curso Java: Trabalhando com lambdas, Streams e Spring Framework

--- src/br/com/alura/screenmatch/modelos/TituloOmdb.java
+++
@@ -1,9 +0,0 @@
-package br.com.alura.screenmatch.modelos;
-
-public record TituloOmdb(String title, String year, String runtime) {
-    /**
-     * Funciona como uma classe especial
-     * Ela já tem implementado um construtor padrão, getter e setter
-     * toString
-     * */
-}
