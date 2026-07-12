a3d8e8eed447e9a61133e47fdba9d7fd22e0390d
Leticia Boldo <leticiamboldo14@gmail.com>
2026-07-12 12:16:42 -0300

    Aula 01: Um novo projeto utilizando o Spring Framework

---
+++ src/main/java/br/com/alura/screenmatch/service/IConverteDados.java
@@ -0,0 +1,7 @@
+package br.com.alura.screenmatch.service;
+
+public interface IConverteDados {
+
+    <T> T obterDados(String json, Class<T> classe); /*Não sei qual entidade vai ser devolvida <T> T*/
+
+}
