cb9d6ae9efd8f432edb2f60d773fa22a68b40a91
Leticia Boldo <leticiamboldo14@gmail.com>
2026-08-06 17:50:28 -0300

    conectando ao front-end e criando a conexão com o controller para trazer as informações do index

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplicationSemWEB.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplicationSemWEB.java
@@ -7,11 +7,23 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
 @SpringBootApplication
-public class ScreenmatchApplication{
+public class ScreenmatchApplicationSemWEB implements CommandLineRunner {
+
+	@Autowired
+	private SerieRepository repositorio;
+
 	public static void main(String[] args) {
 
-		SpringApplication.run(ScreenmatchApplication.class, args);
+		SpringApplication.run(ScreenmatchApplicationSemWEB.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal(repositorio);
+		principal.exibeMenu();
+
+	}
 }
+*/
---
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -0,0 +1,40 @@
+package br.com.alura.screenmatch.controller;
+
+import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.service.SerieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping("/series")
+@RestController
+public class SerieController {
+
+    @Autowired
+    private SerieService servico;
+
+    @GetMapping
+    public List<SerieDTO> obterSeries() {
+        return servico.obterTodasAsSeries();
+    }
+
+    @GetMapping("/top5")
+    public List<SerieDTO> obterTop5Series() {
+        return servico.obterTop5Series();
+    }
+
+    @GetMapping("/lancamentos")
+    public List<SerieDTO> obterLancamento() {
+        return servico.obterLancamento();
+    }
+
+    @GetMapping("/{id}")
+    public SerieDTO obterPorId(@PathVariable Long id) {
+        //@PathVariable serve para informar que essa é uma variavel vinda no path (caminho do endpoint
+        return servico.obterPorId(id);
+    }
+}
